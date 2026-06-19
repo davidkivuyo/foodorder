ff28d6b8b4347cba8ba6618c63fe7449333d2eff
David Larabal <davidkivuyo123@gmail.com>
2026-06-20 00:02:14 +0300

    added hello name text on top of the app and redesigned top appbar code

--- lib/navigation/bottom_navigation.dart
+++ lib/navigation/bottom_navigation.dart
@@ -11,48 +11,6 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _NavigationExampleState();
 }
 
-// The top appbar
-class CustomBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-          margin: const EdgeInsets.only(top: 12.0, left: 8.0, right: 8.0),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'CampusBite',
-                    style: TextStyle(
-                    color: Color.fromARGB(255, 1, 167, 53),
-                    letterSpacing: .5,
-                    fontSize: 20,
-                    fontStyle: FontStyle.normal,
-                  ),
-                ),
-              IconButton(onPressed: () { 
-            Navigator.push(
-              context, 
-              MaterialPageRoute(
-                builder: (_) => Notify(),
-                ),
-                );
-                }, icon: Icon(Icons.notifications)),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(60.0);
-}
-
 
 // notification screen
 class Notify extends StatelessWidget {
@@ -82,7 +40,30 @@ class _NavigationExampleState extends State<MainScreen> {
     return Scaffold(
       
       // TopBar
-      appBar: CustomBar(),
+      appBar: AppBar(
+         title: Text(
+                'CampusBite',
+                    style: TextStyle(
+                    color: Color.fromARGB(255, 1, 167, 53),
+                    letterSpacing: .5,
+                    fontSize: 20,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              actions: <Widget>[
+                IconButton(onPressed: () { 
+            Navigator.push(
+              context, 
+              MaterialPageRoute(
+                builder: (_) => Notify(),
+                ),
+                );
+                }, 
+                icon: Icon(Icons.notifications)
+                ),
+              ]
+            
+      ),
 
       // body
       body: _pages[currentPageIndex],
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+String firstname = 'DAVID';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,8 +15,17 @@ class HomeScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                  Text(
+                     'Hello $firstname',
+                     style: TextStyle(
+                    color: Color.fromARGB(255, 218, 131, 0),
+                    letterSpacing: .5,
+                    fontSize: 20,
+                    fontStyle: FontStyle.normal,
+                     ),
+                  ),
                    Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 12),                 
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
                   child: SearchBar(
                     leading: Icon(Icons.search),
                     hintText: 'Search',
