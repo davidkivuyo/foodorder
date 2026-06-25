0874f321f047296218237d83abeff40b71b3d81a
David Larabal <davidkivuyo123@gmail.com>
2026-06-25 15:20:03 +0300

    craeted food_data.dart file for the food models

---
+++ lib/data/food_data.dart
@@ -0,0 +1,17 @@
+class FoodItem {
+  final String image;
+  final String title;
+  final String subtitle;
+  final String price;
+  final double rating;
+  final String category;
+
+  const FoodItem({
+    required this.image,
+    required this.title,
+    required this.subtitle,
+    required this.price,
+    required this.rating,
+    required this.category,
+  });
+}
