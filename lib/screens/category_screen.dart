65d07c9fc7bee4c9af366850ec35c3267c146016
David Larabal <davidkivuyo123@gmail.com>
2026-06-30 03:21:08 +0300

    deleted items description file and added its content separately to each file

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../data/food_data.dart';
 import '../services/cart_service.dart';
-import 'item_description.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -180,7 +179,7 @@ class FoodCard extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (_) => ItemDescriptions(item: item),
+                    builder: (_) => ItemDescriptionsCategories(item: item),
                   ),
                 );
               },
@@ -342,6 +341,97 @@ class BottomBanner extends StatelessWidget {
                 Icons.celebration, // Using celebration icon as the party popper
                 size: 40,
                 color: Colors.white.withValues(alpha: 0.7),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemDescriptionsCategories extends StatelessWidget {
+  final FoodItem item;
+
+  const ItemDescriptionsCategories({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Hero(
+                  tag: item.title,
+                  child: ClipRRect(
+                    child: Image.asset(
+                      item.image,
+                      width: double.infinity,
+                      height: 220,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.black45,
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back, color: Colors.white),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 24),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    item.title,
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 8),
+
+                  Text(
+                    item.subtitle,
+                    style: const TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  Row(
+                    children: [
+                      const Icon(Icons.star, color: Colors.amber),
+                      const SizedBox(width: 5),
+                      Text("${item.rating}"),
+                      const Spacer(),
+                      Text(
+                        "TZS ${item.price}",
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orange,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:campusbite/screens/item_description.dart';
 import 'package:flutter/material.dart';
 import '../data/food_data.dart';
 import '../data/search_bar.dart';
@@ -417,7 +416,7 @@ class CardRowItems extends StatelessWidget {
                                     context,
                                     MaterialPageRoute(
                                       builder: (_) =>
-                                          ItemDescriptions(item: item),
+                                          ItemDescriptionsHome(item: item),
                                     ),
                                   );
                                 },
@@ -631,6 +630,97 @@ class QuickBites extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ItemDescriptionsHome extends StatelessWidget {
+  final FoodItem item;
+
+  const ItemDescriptionsHome({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Hero(
+                  tag: '${item.title}_${item.image}',
+                  child: ClipRRect(
+                    child: Image.asset(
+                      item.image,
+                      width: double.infinity,
+                      height: 220,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.black45,
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back, color: Colors.white),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 24),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    item.title,
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 8),
+
+                  Text(
+                    item.subtitle,
+                    style: const TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  Row(
+                    children: [
+                      const Icon(Icons.star, color: Colors.amber),
+                      const SizedBox(width: 5),
+                      Text("${item.rating}"),
+                      const Spacer(),
+                      Text(
+                        "TZS ${item.price}",
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orange,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
