e9285a1fef08e073608709985ed3c77b6dc894ab
David Larabal <davidkivuyo123@gmail.com>
2026-07-09 01:32:27 +0300

    Update reset_password.dart

---
+++ lib/screens/reset_password.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ResetPassword extends StatelessWidget {
+  const ResetPassword({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Reset Password Screen',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
