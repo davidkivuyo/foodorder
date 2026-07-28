fc908f644129272603d75b523ab40af8fabc0137
David Larabal <davidkivuyo123@gmail.com>
2026-07-29 01:08:11 +0300

    updated the versionCode and Name in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.campusbite"
         minSdk = 24
         targetSdk = 36
-        // versionCode and versionName come from pubspec.yaml
-        // (overridden by --build-name/--build-number in CI)
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     // Define the release signing configuration
